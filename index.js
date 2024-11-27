4020758b096ed88b4dbd678620be894a30244c60
Abubokkor98 <mail.abubokkor@gmail.com>
2024-11-27 17:17:25 +0600

    55-2 Async Await, Try Catch and Connect to database

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -10,8 +12,8 @@ app.use(express.json());
 // vPemGSl7Fj5eDg9R
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.pnx6a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const uri = "mongodb+srv://abcocofficial:vPemGSl7Fj5eDg9R@cluster0.pnx6a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -31,7 +33,7 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
